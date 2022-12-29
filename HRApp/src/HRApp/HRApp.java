d5a61a8103e4b318dddeb94794cb885affb533b3
andrewtan2000 <andrew_tan2000@hotmail.com>
2022-12-29 21:40:29 +0800

    Initial Commit

---
+++ HRApp/src/HRApp/HRApp.java
@@ -0,0 +1,21 @@
+package HRApp;
+
+public class HRApp {
+	public static void main(String[] args) {
+		System.out.println("HR App starts");
+		Employee e1 = new Employee(230, "Jerry", 333998);
+		Employee e2 = new Employee(420, "Gracie", 3392823);
+		Department dept = new Department("Education");
+		dept.addEmp(e1);
+		dept.addEmp(e2);
+		dept.addEmp(new Employee(772, "Jack", 2998));
+
+		Employee[] emps = dept.getEmployees();
+		for (Employee emp : emps) {
+			System.out.println("Emp " + emp);
+		}
+		System.out.println("Total " + dept.getTotalSalary());
+		System.out.println("Avg " + dept.getAverageSalary());
+		System.out.println("Emp " + dept.getEmployeeByID(420));
+	}
+}
